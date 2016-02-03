61dadec7d47f3c2688a50d73f3327112e1c6c912
dkohlbre <dkohlbre@cs.ucsd.edu>
2016-02-03 11:00:07 -0800

    Added license boilerplate to all added files

--- xpcom/threads/PauseTask.cpp
+++ xpcom/threads/PauseTask.cpp
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include "PauseTask.h"
 #define __STDC_FORMAT_MACROS
 #include <sys/time.h>
